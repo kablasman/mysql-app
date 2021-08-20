9fbce9d2e306c87f60cb5ac771591f312afbded0
Katherine Blasman <b.katherine@hotmail.com>
2021-08-20 13:30:47 -0400

    setting up different paths for enviro variables

--- config/index.js
+++ config/index.js
@@ -1,13 +1,15 @@
-const config = {}
+const config = {};
+require('dotenv').config({path: `.env.${process.env.NODE_ENV}`});
+config.serverPort = process.env.SERVER_PORT;
+console.log("process***", process.env.SERVER_PORT);
 
-config.serverPort = 8080
-
+// setting up server using environment variables, secures data
 config.dbConfig = {
-    user: 'root',
-    password: 'root',
-    host: 'localhost',
-    port: 8889,
-    database: 'nodejs'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
 }
 
 module.exports = config;
